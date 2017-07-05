c5d2f64a8337cd4cc85d461348efa876232bf7e6
bcata6 <b.catalin611@gmail.com>
2017-07-05 18:12:48 +0300

    Split parsing into methods.

--- src/dart/parse.rs
+++ src/dart/parse.rs
@@ -1,98 +1,166 @@
-use std::cell::Cell;
+use std::slice;
 use dart::dsl::*;
 use dart::lex::{Token, stringify};
 
-pub fn parse_dsl(tokens: &[Token]) -> Vec<Item> {
-    let allow_whitespace = Cell::new(false);
-    let mut tokens = tokens.iter().filter(|token| {
-        match **token {
-            Token::WhiteSpace(_) => allow_whitespace.get(),
-            _ => true
-        }
-    });
-    let mut items = vec![];
-    while let Some(token) = tokens.next() {
-        if token.as_ident() == Some("def") {
-            let name = tokens.next().unwrap().as_ident().unwrap().to_string();
-            assert_eq!(*tokens.next().unwrap(), Token::Punctuation('{'));
-            let mut parts = vec![];
-            while let Some(token) = tokens.next() {
-                if *token == Token::Punctuation('}') {
-                    break;
-                }
-                let name = token.as_ident().unwrap().to_string();
-                assert_eq!(*tokens.next().unwrap(), Token::Punctuation('{'));
-                let mut fields = vec![];
-                while let Some(token) = tokens.next() {
-                    if *token == Token::Punctuation('}') {
-                        break;
-                    }
-                    let name = token.as_ident().unwrap().to_string();
-                    assert_eq!(*tokens.next().unwrap(), Token::Punctuation(':'));
-                    let mut expr = vec![];
-                    let mut depth = 0;
-                    let mut ends_in_brace = false;
-                    allow_whitespace.set(true);
-                    while let Some(token) = tokens.next() {
-                        if let Token::Punctuation(c) = *token {
-                            if depth == 0 {
-                                if c == ',' {
-                                    break;
-                                }
-                                if c == '}' {
-                                    ends_in_brace = true;
-                                    break;
-                                }
-                            }
-                            if "{[(".contains(c) {
-                                depth += 1;
-                            }
-                            if "}])".contains(c) {
-                                depth -= 1;
-                            }
-                        }
-                        expr.push(token.clone());
-                    }
-                    allow_whitespace.set(false);
-                    let expr = Expr { dart: stringify(&expr) };
-                    let field = Field {
-                        name: name,
-                        value: expr,
-                    };
-                    fields.push(field);
-                    if ends_in_brace {
-                        break;
-                    }
-                }
-                let comp_part = ComponentPart::Instance(name, fields);
-                parts.push(comp_part);
-            }
-            items.push(Item::ComponentDef(name, parts));
-        }
-        if token.as_ident() == Some("dart") {
-            assert_eq!(*tokens.next().unwrap(), Token::Punctuation('{'));
-            let mut code = vec![];
-            let mut depth = 0;
-            allow_whitespace.set(true);
-            while let Some(token) = tokens.next() {
-                if let Token::Punctuation(c) = *token {
-                    if depth == 0 {
-                        if c == '}' {
-                            break;
-                        }
-                    }
-                    if "{[(".contains(c) {
-                        depth += 1;
-                    }
-                    if "}])".contains(c) {
-                        depth -= 1;
-                    }
-                }
-                code.push(token.clone());
-            }
-            allow_whitespace.set(false);
-            items.push(Item::VerbatimDart(stringify(&code)));
+pub struct Parser<'a> {
+    tokens: slice::Iter<'a, Token>,
+    cur: Option<&'a Token>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(tokens: &'a [Token]) -> Self {
+        let mut tokens = tokens.iter();
+        Parser {
+            cur: tokens.next(),
+            tokens: tokens,
         }
     }
-    items
+
+    fn out_of_tokens(&self) -> bool {
+        self.cur.is_none()
+    }
+
+    fn is_punctuation(&self, c: char) -> bool {
+        if let Some(token) = self.cur {
+            *token == Token::Punctuation(c)
+        } else {
+            false
+        }
+    }
+
+    fn is_keyword(&self, s: &str) -> bool {
+        if let Some(token) = self.cur {
+            token.as_ident() == Some(s)
+        } else {
+            false
+        }
+    }
+
+    fn bump(&mut self) {
+        loop {
+            self.cur = self.tokens.next();
+            match self.cur {
+                Some(&Token::WhiteSpace(_)) => {}
+                _ => break,
+            }
+        }
+    }
+
+    fn eat_punctuation(&mut self, c: char) -> bool {
+        if self.is_punctuation(c) {
+            self.bump();
+            true
+        } else {
+            false
+        }
+    }
+
+    fn eat_keyword(&mut self, s: &str) -> bool {
+        if self.is_keyword(s) {
+            self.bump();
+            true
+        } else {
+            false
+        }
+    }
+
+    fn parse_ident(&mut self) -> String {
+        let ident = self.cur.unwrap().as_ident().unwrap().to_string();
+        self.bump();
+        ident
+    }
+
+    fn parse_dart(&mut self) -> String {
+        let mut depth = 0;
+        let mut code = vec![];
+        while let Some(token) = self.cur {
+            if let Token::Punctuation(c) = *token {
+                match c {
+                    ',' if depth == 0 => break,
+                    '{' | '[' | '(' => depth += 1,
+                    '}' | ']' | ')' => {
+                        if depth == 0 {
+                            break;
+                        }
+                        depth -= 1;
+                    }
+                    _ => {}
+                }
+            }
+            code.push(token.clone());
+            self.cur = self.tokens.next();
+        }
+        stringify(&code)
+    }
+
+    fn parse_expr(&mut self) -> Expr {
+        Expr { dart: self.parse_dart() }
+    }
+
+    fn parse_field(&mut self) -> Field {
+        let name = self.parse_ident();
+        assert!(self.eat_punctuation(':'));
+        Field {
+            name: name,
+            value: self.parse_expr(),
+        }
+    }
+
+    fn parse_fields(&mut self) -> Vec<Field> {
+        let mut fields = vec![];
+        while !self.out_of_tokens() {
+            if self.is_punctuation('}') {
+                break;
+            }
+            fields.push(self.parse_field());
+            if !self.eat_punctuation(',') {
+                break;
+            }
+        }
+        fields
+    }
+
+    fn parse_component_part(&mut self) -> ComponentPart {
+        let name = self.parse_ident();
+        assert!(self.eat_punctuation('{'));
+        let fields = self.parse_fields();
+        assert!(self.eat_punctuation('}'));
+        ComponentPart::Instance(name, fields)
+    }
+
+    fn parse_component_parts(&mut self) -> Vec<ComponentPart> {
+        let mut parts = vec![];
+        while !self.out_of_tokens() {
+            if self.is_punctuation('}') {
+                break;
+            }
+            parts.push(self.parse_component_part());
+        }
+        parts
+    }
+
+    fn parse_item(&mut self) -> Item {
+        if self.eat_keyword("def") {
+            let name = self.parse_ident();
+            assert!(self.eat_punctuation('{'));
+            let parts = self.parse_component_parts();
+            assert!(self.eat_punctuation('}'));
+            Item::ComponentDef(name, parts)
+        } else if self.eat_keyword("dart") {
+            assert!(self.eat_punctuation('{'));
+            let dart = self.parse_dart();
+            assert!(self.eat_punctuation('}'));
+            Item::VerbatimDart(dart)
+        } else {
+            panic!("unknown item");
+        }
+    }
+
+    pub fn parse_items(&mut self) -> Vec<Item> {
+        let mut items = vec![];
+        while !self.out_of_tokens() {
+            items.push(self.parse_item());
+        }
+        items
+    }
 }
