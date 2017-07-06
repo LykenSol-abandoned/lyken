4044846f7aacdeab3a8ca27b1bebfee722f6e04b
rovy <roxxy.prettygirl@gmail.com>
2017-07-06 12:03:51 +0300

    Remove redundant Ok(()).

--- src/dart/codegen.rs
+++ src/dart/codegen.rs
@@ -27,13 +27,12 @@ impl<'a> Codegen<'a> {
                 writeln!(self.out, "Widget build(BuildContext context) {{")?;
                 self.codegen_part(&parts[0])?;
                 writeln!(self.out, "}}")?;
-                writeln!(self.out, "}}")?;
+                writeln!(self.out, "}}")
             }
             Item::VerbatimDart(ref dart) => {
-                writeln!(self.out, "{}", dart)?;
+                writeln!(self.out, "{}", dart)
             }
         }
-        Ok(())
     }
 
     fn codegen_part(&mut self, part: &ComponentPart) -> io::Result<()> {
@@ -41,32 +40,29 @@ impl<'a> Codegen<'a> {
             ComponentPart::Instance(ref instance) => {
                 write!(self.out, "return ")?;
                 self.codegen_instance(&instance)?;
-                writeln!(self.out, ";")?;
+                writeln!(self.out, ";")
             }
             ComponentPart::Field(..) => {
                 unimplemented!();
             }
         }
-        Ok(())
     }
 
     fn codegen_field(&mut self, field: &Field) -> io::Result<()> {
         write!(self.out, "{}: ", field.name)?;
         self.codegen_expr(&field.value)?;
-        writeln!(self.out, ",")?;
-        Ok(())
+        writeln!(self.out, ",")
     }
 
     fn codegen_expr(&mut self, expr: &Expr) -> io::Result<()> {
         match *expr {
             Expr::Instance(ref instance) => {
-                self.codegen_instance(&instance)?;
+                self.codegen_instance(&instance)
             }
             Expr::VerbatimDart(ref dart) => {
-                write!(self.out, "{}", dart)?;
+                write!(self.out, "{}", dart)
             }
         }
-        Ok(())
     }
 
     fn codegen_instance(&mut self, instance: &Instance) -> io::Result<()> {
@@ -74,7 +70,6 @@ impl<'a> Codegen<'a> {
         for field in &instance.fields {
             self.codegen_field(field)?;
         }
-        write!(self.out, ")")?;
-        Ok(())
+        write!(self.out, ")")
     }
 }
