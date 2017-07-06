6b34a9c0b3df8dd86fe0d13607f231d3617e762c
rovy <roxxy.prettygirl@gmail.com>
2017-07-06 14:53:30 +0300

    Lexer state into struct.

--- src/dart/lex.rs
+++ src/dart/lex.rs
@@ -1,4 +1,5 @@
 use unicode_xid::UnicodeXID;
+use std::str;
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Token {
@@ -38,210 +39,224 @@ pub struct ErrorLocation {
     pub line: usize,
 }
 
-pub fn tokenize(src: &str) -> Result<Vec<Token>, ErrorLocation> {
-    let mut chars = src.chars();
-    let mut c = '\n';
-    let mut tokens = vec![];
-    let mut line = 0;
-    struct InterpolationLevel {
-        quote: char,
-        triple: bool,
-        brace_depth: usize,
-    }
-    let mut interpolation_levels: Vec<InterpolationLevel> = vec![];
-    macro_rules! bump {
-    	(or $otherwise:stmt) => {
-            match chars.next() {
-                Some(next) => {
-                    if c == '\n' {
-                        line += 1;
-                    }
-                    c = next
-                }
-                None => {
-                    $otherwise;
-                    return Ok(tokens);
-                }
-            }
-    	};
-        () => {
-            bump!(or {})
-        }
-    }
-    macro_rules! emit {
-        ($e:expr) => (Err(ErrorLocation { err: $e, line })?)
-    }
-    bump!();
-    loop {
-        let mut buffer = String::new();
-        let mut interpolation_before = false;
-        if let Some(last) = interpolation_levels.last_mut() {
-            if c == '{' {
-                last.brace_depth += 1;
-            } else if c == '}' {
-                last.brace_depth -= 1;
-                if last.brace_depth == 0 {
-                    interpolation_before = true;
-                }
-            }
-        }
+pub struct Lexer<'a> {
+    chars: str::Chars<'a>,
+    c: char,
+    tokens: Vec<Token>,
+    line: usize,
+}
 
-        if c.is_numeric() {
-            while c.is_alphanumeric() {
-                buffer.push(c);
-                bump!(or tokens.push(Token::IntegerLiteral(buffer)));
-            }
-            tokens.push(Token::IntegerLiteral(buffer));
-        } else if c.is_whitespace() {
-            while c.is_whitespace() {
-                buffer.push(c);
-                bump!(or tokens.push(Token::WhiteSpace(buffer)));
-            }
-            tokens.push(Token::WhiteSpace(buffer));
-        } else if UnicodeXID::is_xid_start(c) || c == '$' || c == '_' {
-            while UnicodeXID::is_xid_continue(c) || c == '$' {
-                buffer.push(c);
-                bump!(or tokens.push(Token::Identifier(buffer)));
-            }
-            tokens.push(Token::Identifier(buffer));
-        } else if "{}[]()=+-*%&|.,:;!<>@~#?^".contains(c) && !interpolation_before {
-            tokens.push(Token::Punctuation(c));
-            bump!();
-        } else if c == '/' {
-            bump!(or tokens.push(Token::Punctuation(c)));
-            if c == '/' {
-                buffer.push('/');
-                while c != '\n' {
-                    buffer.push(c);
-                    bump!(or tokens.push(Token::WhiteSpace(buffer)));
+impl<'a> Lexer<'a> {
+    pub fn new(src: &'a str) -> Self {
+        Lexer {
+            chars: src.chars(),
+            c: '\n',
+            tokens: vec![],
+            line: 0,
+        }
+    }
+
+    pub fn tokenize(mut self) -> Result<Vec<Token>, ErrorLocation> {
+        struct InterpolationLevel {
+            quote: char,
+            triple: bool,
+            brace_depth: usize,
+        }
+        let mut interpolation_levels: Vec<InterpolationLevel> = vec![];
+
+        macro_rules! bump {
+            (or $otherwise:stmt) => {
+                match self.chars.next() {
+                    Some(next) => {
+                        if self.c == '\n' {
+                            self.line += 1;
+                        }
+                        self.c = next
+                    }
+                    None => {
+                        $otherwise;
+                        return Ok(self.tokens);
+                    }
                 }
-                tokens.push(Token::WhiteSpace(buffer));
-            } else if c == '*' {
-                let mut depth = 0;
-                let unterminated = Error::UnterminatedBlockComment { start: line };
-                buffer.push_str("/*");
-                bump!(or emit!(unterminated));
-                loop {
-                    if c == '*' {
-                        buffer.push(c);
+            };
+            () => {
+                bump!(or {})
+            }
+        }
+        macro_rules! emit {
+            ($e:expr) => (Err(ErrorLocation { err: $e, line: self.line })?)
+        }
+        bump!();
+        loop {
+            let mut buffer = String::new();
+            let mut interpolation_before = false;
+            if let Some(last) = interpolation_levels.last_mut() {
+                if self.c == '{' {
+                    last.brace_depth += 1;
+                } else if self.c == '}' {
+                    last.brace_depth -= 1;
+                    if last.brace_depth == 0 {
+                        interpolation_before = true;
+                    }
+                }
+            }
+
+            if self.c.is_numeric() {
+                while self.c.is_alphanumeric() {
+                    buffer.push(self.c);
+                    bump!(or self.tokens.push(Token::IntegerLiteral(buffer)));
+                }
+                self.tokens.push(Token::IntegerLiteral(buffer));
+            } else if self.c.is_whitespace() {
+                while self.c.is_whitespace() {
+                    buffer.push(self.c);
+                    bump!(or self.tokens.push(Token::WhiteSpace(buffer)));
+                }
+                self.tokens.push(Token::WhiteSpace(buffer));
+            } else if UnicodeXID::is_xid_start(self.c) || self.c == '$' || self.c == '_' {
+                while UnicodeXID::is_xid_continue(self.c) || self.c == '$' {
+                    buffer.push(self.c);
+                    bump!(or self.tokens.push(Token::Identifier(buffer)));
+                }
+                self.tokens.push(Token::Identifier(buffer));
+            } else if "{}[]()=+-*%&|.,:;!<>@~#?^".contains(self.c) && !interpolation_before {
+                self.tokens.push(Token::Punctuation(self.c));
+                bump!();
+            } else if self.c == '/' {
+                bump!(or self.tokens.push(Token::Punctuation (self.c)));
+                if self.c == '/' {
+                    buffer.push('/');
+                    while self.c != '\n' {
+                        buffer.push(self.c);
+                        bump!(or self.tokens.push(Token::WhiteSpace(buffer)));
+                    }
+                    self.tokens.push(Token::WhiteSpace(buffer));
+                } else if self.c == '*' {
+                    let mut depth = 0;
+                    let unterminated = Error::UnterminatedBlockComment { start: self.line };
+                    buffer.push_str("/*");
+                    bump!(or emit!(unterminated));
+                    loop {
+                        if self.c == '*' {
+                            buffer.push(self.c);
+                            bump!(or emit!(unterminated));
+                            if self.c == '/' {
+                                if depth == 0 {
+                                    break;
+                                }
+                                depth -= 1;
+                            } else {
+                                continue;
+                            }
+                        } else if self.c == '/' {
+                            buffer.push(self.c);
+                            bump!(or emit!(unterminated));
+                            if self.c == '*' {
+                                depth += 1;
+                            } else {
+                                continue;
+                            }
+                        }
+
+                        buffer.push(self.c);
                         bump!(or emit!(unterminated));
-                        if c == '/' {
-                            if depth == 0 {
+                    }
+                    buffer.push('/');
+                    self.tokens.push(Token::WhiteSpace(buffer));
+                    bump!();
+
+                } else {
+                    self.tokens.push(Token::Punctuation('/'));
+                }
+            } else if self.c == '\'' || self.c == '"' || interpolation_before {
+
+                let mut quote = self.c;
+                let mut raw = false;
+                let mut triple = false;
+                let mut interpolation_after = false;
+                match self.tokens.last() {
+                    Some(&Token::Identifier(ref last)) => {
+                        if *last == "r" {
+                            raw = true;
+                        }
+                    }
+                    Some(&Token::StringLiteral {
+                        ref contents,
+                        raw: prev_raw,
+                        triple: false,
+                        quote: prev_quote,
+                        interpolation_after: false,
+                        interpolation_before: false,
+                    }) => {
+                        if contents.is_empty() && prev_quote == quote {
+                            triple = true;
+                            raw = prev_raw;
+                        }
+                    }
+                    _ => {}
+                }
+                if raw || triple {
+                    self.tokens.pop();
+                }
+                if interpolation_before {
+                    let original = interpolation_levels.pop().unwrap();
+                    quote = original.quote;
+                    triple = original.triple;
+                }
+                bump!(or emit!(Error::UnterminatedStringLiteral));
+                loop {
+                    if self.c == quote {
+                        if !triple {
+                            break;
+                        }
+                        bump!(or emit!(Error::UnterminatedStringLiteral));
+                        if self.c == quote {
+                            bump!(or emit!(Error::UnterminatedStringLiteral));
+                            if self.c == quote {
                                 break;
                             }
-                            depth -= 1;
-                        } else {
-                            continue;
-                        }
-                    } else if c == '/' {
-                        buffer.push(c);
-                        bump!(or emit!(unterminated));
-                        if c == '*' {
-                            depth += 1;
-                        } else {
-                            continue;
-                        }
-                    }
-
-                    buffer.push(c);
-                    bump!(or emit!(unterminated));
-                }
-                buffer.push('/');
-                tokens.push(Token::WhiteSpace(buffer));
-                bump!();
-
-            } else {
-                tokens.push(Token::Punctuation('/'));
-            }
-        } else if c == '\'' || c == '"' || interpolation_before {
-
-            let mut quote = c;
-            let mut raw = false;
-            let mut triple = false;
-            let mut interpolation_after = false;
-            match tokens.last() {
-                Some(&Token::Identifier(ref last)) => {
-                    if *last == "r" {
-                        raw = true;
-                    }
-                }
-                Some(&Token::StringLiteral {
-                    ref contents,
-                    raw: prev_raw,
-                    triple: false,
-                    quote: prev_quote,
-                    interpolation_after: false,
-                    interpolation_before: false,
-                }) => {
-                    if contents.is_empty() && prev_quote == quote {
-                        triple = true;
-                        raw = prev_raw;
-                    }
-                }
-                _ => {}
-            }
-            if raw || triple {
-                tokens.pop();
-            }
-            if interpolation_before {
-                let original = interpolation_levels.pop().unwrap();
-                quote = original.quote;
-                triple = original.triple;
-            }
-            bump!(or emit!(Error::UnterminatedStringLiteral));
-            loop {
-                if c == quote {
-                    if !triple {
-                        break;
-                    }
-                    bump!(or emit!(Error::UnterminatedStringLiteral));
-                    if c == quote {
-                        bump!(or emit!(Error::UnterminatedStringLiteral));
-                        if c == quote {
-                            break;
+                            buffer.push(quote);
                         }
                         buffer.push(quote);
                     }
-                    buffer.push(quote);
-                }
-                if c == '\n' && !triple {
-                    emit!(Error::UnterminatedStringLiteral)
-                }
-                if c == '$' && !raw {
-                    bump!(or emit!(Error::UnterminatedStringLiteral));
-                    if c == '{' {
-                        interpolation_levels.push(InterpolationLevel {
-                            quote,
-                            triple,
-                            brace_depth: 1,
-                        });
-                        interpolation_after = true;
-                        break;
+                    if self.c == '\n' && !triple {
+                        emit!(Error::UnterminatedStringLiteral)
                     }
-                    buffer.push('$');
-                    continue;
-                }
-                if c == '\\' && !raw {
-                    buffer.push(c);
+                    if self.c == '$' && !raw {
+                        bump!(or emit!(Error::UnterminatedStringLiteral));
+                        if self.c == '{' {
+                            interpolation_levels.push(InterpolationLevel {
+                                quote,
+                                triple,
+                                brace_depth: 1,
+                            });
+                            interpolation_after = true;
+                            break;
+                        }
+                        buffer.push('$');
+                        continue;
+                    }
+                    if self.c == '\\' && !raw {
+                        buffer.push(self.c);
+                        bump!(or emit!(Error::UnterminatedStringLiteral));
+                    }
+                    buffer.push(self.c);
                     bump!(or emit!(Error::UnterminatedStringLiteral));
                 }
-                buffer.push(c);
-                bump!(or emit!(Error::UnterminatedStringLiteral));
+                self.tokens.push(Token::StringLiteral {
+                    contents: buffer,
+                    raw,
+                    triple,
+                    quote,
+                    interpolation_after,
+                    interpolation_before,
+                });
+                bump!();
+            } else {
+                emit!(Error::UnhandledCharacter(self.c))
             }
-            tokens.push(Token::StringLiteral {
-                contents: buffer,
-                raw,
-                triple,
-                quote,
-                interpolation_after,
-                interpolation_before,
-            });
-            bump!();
-        } else {
-            emit!(Error::UnhandledCharacter(c))
         }
     }
-
 }
 
 pub fn stringify(tokens: &[Token]) -> String {
