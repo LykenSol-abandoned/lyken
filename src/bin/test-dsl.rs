fd40e87c8fe7ae5a72f14cc4e67e7b7813f0e265
bcata6 <b.catalin611@gmail.com>
2017-08-14 17:29:47 +0300

    Update DSL parser to be more like the Dart parser.

--- src/bin/test-dsl.rs
+++ src/bin/test-dsl.rs
@@ -15,11 +15,23 @@ fn main() {
     let file = lyken::codemap().load_file(&path).unwrap();
     match Lexer::new(lyken::mk_sp(file.start_pos, file.end_pos)).tokenize() {
         Ok(tokens) => {
-            let items = Parser::new(tokens.iter().cloned()).parse_items();
-            let mut out = File::create(path.with_extension("dart")).unwrap();
-            let result = Codegen::new().codegen_items(&items);
-            for token in result {
-                write!(out, "{}", token).unwrap();
+            match Parser::new(tokens.iter().cloned()).parse_items() {
+                Ok(items) => {
+                    let mut out = File::create(path.with_extension("dart")).unwrap();
+                    let result = Codegen::new().codegen_items(&items);
+                    for token in result {
+                        write!(out, "{}", token).unwrap();
+                    }
+                }
+                Err(Error(ErrorKind::ExpectedAt { expected, span }, state)) => {
+                    println!("{:?}: expected {:?}", span, expected);
+                    if let Some(ref backtrace) = state.backtrace {
+                        println!("{:?}", backtrace);
+                    }
+                }
+                Err(error) => {
+                    println!("{}: {:?}", path.display(), error);
+                }
             }
         }
         Err(error) => {
