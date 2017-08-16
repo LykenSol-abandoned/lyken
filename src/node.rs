745620edc3722a8c610eba40445aad5466114686
bcata6 <b.catalin611@gmail.com>
2017-08-16 15:22:03 +0300

    Add NodeMap and node_field! in node.

--- src/node.rs
+++ src/node.rs
@@ -1,9 +1,19 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
 use std::fmt;
 use std::ops::Deref;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 pub struct Node<T> {
     ptr: Rc<T>,
+}
+
+impl<T> Clone for Node<T> {
+    fn clone(&self) -> Self {
+        Node {
+            ptr: self.ptr.clone(),
+        }
+    }
 }
 
 impl<T> Deref for Node<T> {
@@ -24,6 +34,92 @@ impl<T> Node<T> {
     pub fn new(value: T) -> Self {
         Node {
             ptr: Rc::new(value),
+        }
+    }
+}
+
+pub struct NodeMap<V> {
+    map: Rc<RefCell<HashMap<OpaqueKey, V>>>,
+}
+
+type OpaqueKey = *const ();
+
+fn node_key<T>(node: &Node<T>) -> OpaqueKey {
+    &**node as *const T as OpaqueKey
+}
+
+thread_local!(static NODE_MAPS: RefCell<Vec<Rc<NodeRemove>>> = RefCell::new(vec![]));
+
+trait NodeRemove {
+    fn remove_node(&self, key: OpaqueKey);
+}
+
+impl<V> NodeRemove for RefCell<HashMap<OpaqueKey, V>> {
+    fn remove_node(&self, key: OpaqueKey) {
+        let value = self.borrow_mut().remove(&key);
+        drop(value);
+    }
+}
+
+impl<T> Drop for Node<T> {
+    fn drop(&mut self) {
+        if Rc::strong_count(&self.ptr) == 1 {
+            NODE_MAPS.with(|maps| for map in maps.borrow().iter() {
+                map.remove_node(node_key(self));
+            })
+        }
+    }
+}
+
+impl<V: 'static + Clone> NodeMap<V> {
+    pub fn new() -> Self {
+        let map = Rc::new(RefCell::new(HashMap::new()));
+        NODE_MAPS.with(|maps| maps.borrow_mut().push(map.clone()));
+        NodeMap { map }
+    }
+
+    pub fn entry<'a, T>(&self, key: &'a Node<T>) -> NodeMapEntry<'a, T, V> {
+        NodeMapEntry {
+            key,
+            map: Rc::downgrade(&self.map),
+        }
+    }
+
+    pub fn get<T>(&self, key: &Node<T>) -> Option<V> {
+        self.entry(key).get()
+    }
+
+    pub fn set<T>(&self, key: &Node<T>, value: V) {
+        self.entry(key).set(value)
+    }
+}
+
+pub struct NodeMapEntry<'a, T: 'a, V> {
+    key: &'a Node<T>,
+    map: Weak<RefCell<HashMap<OpaqueKey, V>>>,
+}
+
+impl<'a, T, V: Clone> NodeMapEntry<'a, T, V> {
+    pub fn get(&self) -> Option<V> {
+        let map = self.map.upgrade().unwrap();
+        let value = map.borrow().get(&node_key(self.key)).cloned();
+        value
+    }
+
+    pub fn set(&self, value: V) {
+        let map = self.map.upgrade().unwrap();
+        let prev = map.borrow_mut().insert(node_key(self.key), value);
+        drop(prev);
+    }
+}
+
+macro_rules! node_field {
+    ($name:ident: $ty:ty) => {
+        impl<T> ::node::Node<T> {
+            pub fn $name(&self) -> ::node::NodeMapEntry<T, $ty> {
+                thread_local!(static MAP: ::node::NodeMap<$ty> = ::node::NodeMap::new());
+                MAP.with(|map| map.entry(self))
+            }
         }
     }
 }
